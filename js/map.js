9461ed58b62939d7349954987a32d7cc1e7a6e42
Lindsay Huth <huthlindsay@gmail.com>
2018-10-22 16:00:33 -0400

    trying very hard to get this cross-origin request to work and it refuses

---
+++ js/map.js
@@ -0,0 +1,17 @@
+/*
+var svg = d3.select('#map-svg')
+
+d3.json("https://elections.maryland.gov/elections/results_data/GG18/Results.js", function(error, data) {
+
+    data.forEach(function(d) {
+        d.Results = d.Results;
+        d.DReporting = d.DReporting;
+        d.Reporting = d.Reporting;
+        d.EVReporting = d.EVReporting;
+        d.CanvasCounts = d.CanvasCounts;
+    });
+
+    console.log(d.Results);
+
+  });
+*/
---
+++ js/parse-results.js
@@ -0,0 +1,3 @@
+var data = "https://elections.maryland.gov/elections/results_data/GG18/Results.js";
+
+console.log(data);
