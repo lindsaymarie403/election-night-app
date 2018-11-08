7392a642868d5a2af8d1c5f629139f66a896fb1f
Lindsay Huth <huthlindsay@gmail.com>
2018-11-08 10:48:56 -0500

    graphic 4 working

--- election-night-graphics/graphics/graphic4/graphic4.js
+++ election-night-graphics/graphics/graphic4/graphic4.js
@@ -1,7 +1,7 @@
 
-var margin = {top: 10, right: 35, bottom: 40, left: 105},
+var margin = {top: 30, right: 35, bottom: 40, left: 105},
     width = 600 - margin.left - margin.right,
-    height = 600 - margin.top - margin.bottom;
+    height = 500 - margin.top - margin.bottom;
 
 var svg = d3.select("#chart").append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -9,25 +9,23 @@ var svg = d3.select("#chart").append("svg")
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var total = ["Rep", "Hogan"];
-
 var x = d3.scaleLinear().rangeRound([0, width]),
     y = d3.scaleBand().rangeRound([0, height]).padding(0.35),
     z = d3.scaleOrdinal().range(["#f04b4d", "#f5898b"]);
 
 // load data
 d3.csv("data.csv", function(d, i, columns) {
-		for (i = 3, t = 0; i < 6; ++i) t += d[columns[i]] = +d[columns[i]];
+		for (i = 1, t = 0; i < columns.length; ++i) t += d[columns[i]] = +d[columns[i]];
 		d.total = t;
 		return d;
 }, function(error, data) {
 		if (error) throw error;
 
-	var keys = data.columns.slice(3,6);
+	var keys = data.columns.slice(1);
 
   console.log(data);
 
-  x.domain([0,150000]);
+  x.domain([0,200000]);
   y.domain(data.map(function(d) { return d.County; }));
   z.domain(keys);
   //y.domain([d3.min(data, function(d) { return d.year; }), d3.max(data, function(d) { return d.year; })]);
@@ -41,48 +39,40 @@ d3.csv("data.csv", function(d, i, columns) {
     .selectAll("rect")
     .data(function(d) { return d; })
     .enter().append("rect")
+      .attr("x", function(d) { return x(d[0]); })
       .attr("y", function(d) { return y(d.data.County); })
-      .attr("x", function(d) { return x(d[1]); })
-      .attr("width", function(d) { return x(d[0]) - x(d[1]); })
+      .attr("width", function(d) { return x(d[1]) - x(d[0]); })
       .attr("height", y.bandwidth());
 
-/*
-  var layer = svg.selectAll(".layer")
-        .data(layers)
-      .enter().append("g")
-        .attr("class", "layer")
-        .style("fill", function(d, i) { return z(i); });
-
-    layer.selectAll("rect")
-        .data(function(d) { return d; })
-      .enter().append("rect")
-        .attr("x", function(d) { return x(d.x + d.x0); })
-        .attr("y", function(d) { return y(d.y); })
-        .attr("height", y.bandwidth())
-        .attr("width", function(d) { return x(d.x0) - x(d.x + d.x0); });
-        */
-/*
-  svg.selectAll(".bar")
+  svg.append("text")
     .data(data)
-    .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", 0)
-      .attr("y", function(d) { return y(d.county); })
-      .attr("width", function(d) {return x(d.pctchg); } )
-      .attr("height", y.bandwidth())
-      .attr("fill", function(d) {
-           if (d.year == 2018) {
-             return "#d3d3d3";
-           } return "#f04b4d";
-       });
-*/
+     .attr("class","label")
+     .attr("x", (function(d) { return x(0); }  ))
+     .attr("y", 5)
+     .text("Republican voters")
+      .style("text-anchor","start")
+      .attr("font-size", "10px")
+      .attr("font-family","Poppins")
+      .attr("font-weight", "600");
+
+  svg.append("text")
+    .data(data)
+     .attr("class","label2")
+     .attr("x", (function(d) { return x(142739); }  ))
+     .attr("y", 5)
+     .text("Votes for Hogan")
+      .style("text-anchor","start")
+      .attr("font-size", "10px")
+      .attr("font-family","Poppins")
+      .attr("font-weight", "600");
+
   svg.append("g")
       .attr("class", "x-axis")
       .attr("transform", "translate(0," + height + ")")
       .style("font-family","Poppins")
       .style("font-size", "12px")
       .style("color", "#3a3a3a")
-      .call(d3.axisBottom(x).tickValues([0,50000,100000,150000]).tickSize(0).tickPadding(8));
+      .call(d3.axisBottom(x).tickValues([0,50000,100000,150000,200000]).tickSize(0).tickPadding(8));
       //.select(".domain")
         //.remove();
 
@@ -95,6 +85,7 @@ d3.csv("data.csv", function(d, i, columns) {
     .call(d3.axisLeft(y).ticks(3).tickSize(0).tickPadding(8))
     .select(".domain")
       .remove();
+
 /*
    svg.selectAll(".text")
      .data(data)
@@ -102,13 +93,14 @@ d3.csv("data.csv", function(d, i, columns) {
       .append("text")
       .attr("class","label")
       .attr("x", (function(d) { return x(d.pctchg) + 15; }  ))
-      .attr("y", function(d) { return y(d.county) + y.bandwidth()/2 + 3; })
+      .attr("y", function(d) { return y(d.data.County) + y.bandwidth()/2 + 3; })
       .text(function(d) { return d.pctchg; })
        .style("text-anchor","middle")
        .attr("font-size", "10px")
        .attr("font-family","Poppins")
        .attr("font-weight", "600");
-*/
+  */
+
 });
 
 
@@ -129,13 +121,18 @@ function resize() {
 
   d3.select('svg')
       .attr('width', (width + margin.left + margin.right) + 'px');
-/*
-  svg.selectAll(".bar")
-      .attr("width", function(d) {return x(d.pctchg); } );
-*/
+
+  svg.selectAll("rect")
+      .attr("x", function(d) { return x(d[0]); })
+      .attr("width", function(d) { return x(d[1]) - x(d[0]); });
+
   svg.selectAll(".x-axis")
-      .call(d3.axisBottom(x).tickValues([0,50000,100000,150000]).tickSize(0).tickPadding(8));
+      .call(d3.axisBottom(x).tickValues([0,50000,100000,150000, 200000]).tickSize(0).tickPadding(8));
 
   svg.selectAll(".label")
-      .attr("x", (function(d) { return x(d.pctchg) + 15; }  ));
+      .attr("x", (function(d) { return x(0); } ));
+
+  svg.selectAll(".label2")
+      .attr("x", (function(d) { return x(142739); } ));
+
 };
